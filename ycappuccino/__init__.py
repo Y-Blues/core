c22cc365a7b1731cd3043c546be0160dad5e6047
aurelien.pisu <aurelien.pisu@gmail.com>
2023-08-17 11:45:42 +0200

    refactoring

--- ycappuccino/__init__.py
+++
@@ -1,2 +0,0 @@
-# YCappuccino ycappuccino.core default need to have a interaction with client
-
---
+++ ycappuccino/setup.py
@@ -0,0 +1,21 @@
+"""Setup file for qsolverlib."""
+
+import setuptools
+
+setuptools.setup(
+    name="ycappuccino.core",
+    version="0.0.1",
+    description="A framewore core library for declaring component based software app",
+    url="https://github.com/Y-Blues/core",
+    author="Aurelien Pisu",
+    author_email="aurelie.pisu@gmail.com",
+    license=None,
+    classifiers=[
+        "Programming Language :: Python"
+    ],
+    install_requires=[
+        'ipopo',
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9"
+)
